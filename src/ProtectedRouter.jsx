fba6625051ff9c879239ace5f788b7fe4395007e
CristianAguilarOficial <aguilarcristian060@gmail.com>
2025-04-21 11:04:08 -0500

    agregando el importacion cosnt navigate XD

--- src/ProtectedRouter.jsx
+++ src/ProtectedRouter.jsx
@@ -1,8 +1,9 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useNavigate } from "react-router-dom";
 import { useAuth } from "./context/authContext";
 import { useEffect } from "react";
 
 function ProtectedRouter() {
+  const navigate = useNavigate();
   const { loading, isAuthenticated } = useAuth();
 
   useEffect(() => {
@@ -13,7 +14,11 @@ function ProtectedRouter() {
   }, [loading, isAuthenticated]);
 
   if (loading)
-    return <h1 className="text-white font-bold size-1.5">loading.....</h1>;
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-black">
+        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-white"></div>
+      </div>
+    );
 
   if (!loading && !isAuthenticated) return <Navigate to="/login" replace />;
 
